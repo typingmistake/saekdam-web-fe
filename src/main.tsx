d0d39a81b801d5c66abc5823bf0f50a35b93de31
선빈 <ehtjsqls@gmail.com>
2025-02-12 11:38:45 +0900

    feat: toast ui 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from '@/components/ui/toaster';
 
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
+            <Toaster />
             <App />
         </BrowserRouter>
     </StrictMode>,
