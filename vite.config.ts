9bb509089e43a62af16e546174582087753ff210
선빈 <ehtjsqls@gmail.com>
2025-02-12 17:25:04 +0900

    feat: VITE_ENV 로드 방식을 개선하여 환경 설정 유연성 향상

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vite/client" />
-import { defineConfig, UserConfigExport, ProxyOptions } from 'vite';
+import { defineConfig, loadEnv, ProxyOptions } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
@@ -7,26 +7,28 @@ interface ProxyConfig {
     [key: string]: ProxyOptions;
 }
 
-const env: string = import.meta.env.VITE_ENV || 'development';
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+    const currentEnv = env.VITE_ENV || 'development';
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
+    return {
+        plugins: [react()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+            },
         },
-    },
-    server: {
-        proxy:
-            env === 'development'
-                ? ({
-                      '/api': {
-                          target: 'http://localhost:8080',
-                          changeOrigin: true,
-                          rewrite: (path: string) => path.replace(/^\/api/, ''),
-                      },
-                  } as ProxyConfig)
-                : undefined,
-    },
-} as UserConfigExport);
+        server: {
+            proxy:
+                currentEnv === 'development'
+                    ? ({
+                          '/api': {
+                              target: 'http://localhost:8080',
+                              changeOrigin: true,
+                              rewrite: (path: string) => path.replace(/^\/api/, ''),
+                          },
+                      } as ProxyConfig)
+                    : undefined,
+        },
+    };
+});
